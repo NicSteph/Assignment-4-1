fc1b8fda4a99537a9c691632fed2456e86593096
NicSteph <nstephenson5@hawkmail.hccfl.edu>
2025-10-19 13:14:06 -0400

    Add additional charges based on wood type and color

--- main.py
+++ main.py
@@ -11,5 +11,10 @@ woodType = "oak"
 # Write assignment and if statements here as appropriate.
 if numChars > 5:
   charge = charge + (numChars - 5) * 4
+  if woodType == "oak":
+  charge = charge + 20
+  if color == "gold":
+    charge = charge + 15
+
 # Output Charge for this sign.
 print("The charge for this sign is $" + str(charge))
